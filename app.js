b447a3984cb47d0ffb0cb859e49c68c727036d23
stepan_chepil <chepillstepan11@gmail.com>
2026-08-15 14:56:18 +0300

    multer setup

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const path = require("path");
-const dns = require("dns");
+
 const app = express();
 const cors = require("cors");
 require("dotenv").config()
 
 const PORT = 3000;
 
+const path = require("path");
+
+const bodyParser = require("body-parser");
+
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname,"admin")))
+app.use(express.static(path.join(__dirname,"admin")));
+app.use("/uploads",express.static(path.join(__dirname,"uploads")))
+
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: function(req,file,cb) {
+        cb(null,"uploads/");
+    },
+
+    filename: function(req,file,cb) {
+        cb(null,`${Date.now()}-${file.originalname}`);
+    }
+})
+
+const uplaod = multer({storage: storage});
+
+const dns = require("dns");
 
 dns.setServers([
     `1.1.1.1`,
     `8.8.8.8`
 ])
+
+const mongoose = require("mongoose");
 
 mongoose.connect(process.env.DATABASE_URL)
 .then(()=>{
@@ -35,6 +55,26 @@ const productSchema = new mongoose.Schema({
 })
 
 const Product = mongoose.model("Product", productSchema)
+
+app.post("/upload",uplaod.single("file") , (req,res) => {
+    console.log(req.file);
+
+    res.send(`File sucsessful uploaded!`)
+})
+
+app.get("/images",(req,res) => {
+    const fs = require("fs");
+    const dir = path.join(__dirname,"uploads");
+
+    fs.readdir(dir, (err,files) => {
+        if (err) {
+            return res.status(500).send(`Unable to read file!`);
+        }
+
+        const imageUrl = files.map(file => `/uploads/${file}`);
+        res.json(imageUrl);
+    })
+})
 
 app.get("/products",async (req,res) => {
     const products = await Product.find(req.query);
